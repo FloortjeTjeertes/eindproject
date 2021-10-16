dd168450fe7196b6590c626a838613ebcb0b4ac1
floriantjeertes <florian.tjeertes@gmail.com>
2021-10-16 21:01:55 +0200

    hamburger

--- scripts/general/header.js
+++ scripts/general/header.js
@@ -1,5 +1,24 @@
 let buttons = document.getElementsByClassName("NavButton");
+let hamburger = document.getElementsByClassName("hamburger");
+let navbuttons = document.getElementsByClassName("navbuttons");
 var locations = ["index","bestdestinations","tickets","contact"];
+var active = false;
+
+function hamburgerToggle(){
+    console.log(active);
+    if(active){
+        navbuttons[0].classList.add("hidden");
+        active=false;
+    }
+    else if(!active){
+        navbuttons[0].classList.remove("hidden");
+        active=true;
+    }
+}
+
+hamburger[0].addEventListener("click",function(){
+hamburgerToggle()});
+
 
 for(let i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",function f() {
