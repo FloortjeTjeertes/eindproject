7aac2cce27e0e4e326bb63cc10db2c42a9d7d607
floriantjeertes <florian.tjeertes@gmail.com>
2021-10-31 17:56:01 +0100

    cleanup

--- scripts/general/header.js
+++ scripts/general/header.js
@@ -1,33 +1,31 @@
 let buttons = document.getElementsByClassName("NavButton");
 let hamburger = document.getElementsByClassName("hamburger");
 let navbuttons = document.getElementsByClassName("navbuttons");
-var locations = ["index","bestdestinations","tickets","contact"];
+var locations = ["index", "bestdestinations", "tickets", "contact"];
 var active = false;
 
-function hamburgerToggle(){
-    console.log(active);
-    if(active){
-        navbuttons[0].classList.add("hidden");
-        active=false;
-    }
-    else if(!active){
-        navbuttons[0].classList.remove("hidden");
-        active=true;
-    }
+function hamburgerToggle() {
+  console.log(active);
+  if (active) {
+    navbuttons[0].classList.add("hidden");
+    active = false;
+  } else if (!active) {
+    navbuttons[0].classList.remove("hidden");
+    active = true;
+  }
 }
 
-hamburger[0].addEventListener("click",function(){
-hamburgerToggle()});
+hamburger[0].addEventListener("click", function () {
+  hamburgerToggle();
+});
 
-
-for(let i=0;i<buttons.length;i++){
-    buttons[i].addEventListener("click",function f() {
-        console.log(buttons[i]);
-        for(var j =0;j<buttons.length;j++){
-        buttons[j].classList.remove("active");
-        }
-        buttons[i].className="active";
-        window.location = locations[i]+".html";
-
-    });
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function f() {
+    console.log(buttons[i]);
+    for (var j = 0; j < buttons.length; j++) {
+      buttons[j].classList.remove("active");
+    }
+    buttons[i].className = "active";
+    window.location = locations[i] + ".html";
+  });
 }
